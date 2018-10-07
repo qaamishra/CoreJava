4517fbdaba5b46b81f5c0641c31676b74478bce1
qa.amishra@gmail.com <qa.amishra@gmail.com>
2018-10-07 18:56:12 +0530

    String Tricky

---
+++ src/main/java/problems/stringproblems/StringTricky.java
@@ -0,0 +1,10 @@
+package problems.stringproblems;
+
+public class StringTricky {
+
+    public static void main(String[] args) {
+
+        String s=20+40+40+"abc"+401+1+50;
+        System.out.println(s);
+    }
+}
